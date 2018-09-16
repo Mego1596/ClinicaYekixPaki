d7c840bfbdf4fec34513049143f9483e2b62eaf2
Mego1596 <maurigranados1596@gmail.com>
2018-09-16 09:29:51 -0600

    cambios funcionales en role.show

--- resources/views/roles/show.blade.php
+++ resources/views/roles/show.blade.php
@@ -29,16 +29,32 @@
 					</div>
 					<div class="card-body">
 						<p><strong>Nombre: </strong><br>{{ $role->name }}</p>
-						<p><strong>Slug: </strong><br>{{ $role->slug }}</p>
 						<p align="justify"><strong>Descripcion: </strong><br>{{ $role->description }}</p>
 						<table style="margin-left:10px ">
-						@foreach($permisos as $permiso)
-							<tr>
-								<td>
-									<li>{{$permiso->name}}</li>
-								</td>
-							</tr>	
-						@endforeach
+						@if(sizeof($permisos) == 0)
+							@if($role->id == 1)
+								<tr>
+									<td>
+									<li>Acceso Total</li>
+									</td>
+								</tr>
+							@elseif($role->id == 4)
+								<tr>
+									<td>
+										<li>Ningun Acceso</li>
+									</td>
+								</tr>
+							@else
+							@endif
+						@else
+							@foreach($permisos as $permiso)
+								<tr>
+									<td>
+										<li>{{$permiso->name}}</li>
+									</td>
+								</tr>	
+							@endforeach
+						@endif
 						</table>
 					</div>
 				</div>
