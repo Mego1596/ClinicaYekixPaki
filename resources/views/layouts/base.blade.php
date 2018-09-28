bc7ed49bf1a2df3c0229bec06c2710af9068adc2
Mego1596 <maurigranados1596@gmail.com>
2018-09-28 08:39:58 -0600

    Retiro de Copyright en footer

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -221,7 +221,7 @@
       <footer class="sticky-footer">
           <div class="container my-auto">
             <div class="copyright text-center my-auto">
-              <span>Copyright © Clinica Dental de Atencion Integral y Preventiva Yekixpaki 2018 </span>
+              <span> Clinica Dental de Atencion Integral y Preventiva Yekixpaki 2018 </span>
             </div>
           </div>
         </footer>
