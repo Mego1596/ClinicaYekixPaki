7eb3fa2001e04e4605a7affe36f20f03137c4ecf
Mego1596 <maurigranados1596@gmail.com>
2018-10-10 10:31:56 -0600

    Responsive App V.2
    Campos Responsive de la gestion de usuarios Login, Reset Password, Email

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -17,10 +17,10 @@
                     <form method="POST" action="{{ route('password.email') }}" aria-label="{{ __('Reset Password') }}">
                         @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
+                        <div class="form-group">
+                            <label for="email" class="col-md-4 col-form-label">{{ __('E-Mail') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-12 col-sm-12">
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
 
                                 @if ($errors->has('email'))
@@ -31,10 +31,10 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-success">
-                                    {{ __('Enviar Link de Restablecimiento de Contraseña') }}
+                        <div class="form-group">
+                            <div class="col-md-5 col-sm-12">
+                                <button type="submit" class="btn btn-success" style="width: 100%">
+                                    {{ __('Restablecer de Contraseña') }}
                                 </button>
                             </div>
                         </div>
