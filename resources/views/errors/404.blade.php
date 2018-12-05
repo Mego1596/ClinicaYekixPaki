9538a3dd6d4fd3c3c231115f7adef8608c18e6dc
Marvin Alexander <marvinalexz@gmail.com>
2018-12-05 12:25:06 -0600

    Modo produccion y Vistas de error

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.base')
+
+@section('content')
+<div class="row">
+		<div class="col-md-12">
+			<div class="card">
+				
+				<div class="card-body justify-content-center">
+
+                <div class="alert alert-danger">
+                    <strong>¡Error!</strong> Esta página no está disponible
+                </div>
+				</div>
+			</div>
+		</div>
+	</div>
+@endsection
